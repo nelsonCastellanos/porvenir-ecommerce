9132dd406b8dfe59d82861afa4feaa0d7dfa9184
Nelson Castellanos <nelson.castellanos@mercadolibre.com.co>
2023-07-21 23:58:21 -0500

    <type>: <description>
    <body>
    
    <footer>

--- update-dependencies.js
+++ update-dependencies.js
@@ -10,50 +10,50 @@ const dependencies = Object.keys(packageJson.dependencies || {});
 const devDependencies = Object.keys(packageJson.devDependencies || {});
 
 // Lista de dependencias a ignorar (puedes agregar las que desees)
-const ignoreDependencies = ["history"];
+const ignoreDependencies = ["@fontsource/poppins","autosuggest-highlight","aws-sdk","axios","bcryptjs","bootstrap","compression","connected-react-router","cors","crypto","dompurify","dotenv","express","font-awesome","helmet","history","jsonwebtoken","mailchimp-api-v3","mailgun-js","mobile-detect","mongoose","mongoose-slug-generator","multer","node-sass","passport","passport-facebook","passport-google-oauth2","passport-jwt","rc-slider"];
 
 // Combinar todas las dependencias
 const allDependencies = [...dependencies, ...devDependencies];
 
 // Filtrar las dependencias ignoradas
 const dependenciesToUpdate = allDependencies.filter(
-  (dependency) => !ignoreDependencies.includes(dependency)
+    (dependency) => !ignoreDependencies.includes(dependency)
 );
 
 
 // Función para actualizar una dependencia y continuar con la siguiente
 const updateDependency = async (index) => {
-  if (index >= dependenciesToUpdate.length) {
-    console.log("Todas las dependencias se han actualizado.");
-    return;
-  }
+    if (index >= dependenciesToUpdate.length) {
+        console.log("Todas las dependencias se han actualizado.");
+        return;
+    }
 
-  const dependency = dependenciesToUpdate[index];
+    const dependency = dependenciesToUpdate[index];
 
-  // Obtener información sobre las versiones disponibles del paquete
-  try {
-    const response = await axios.get(`https://registry.npmjs.org/${dependency}`);
-    const versions = Object.keys(response.data.versions || {});
+    // Obtener información sobre las versiones disponibles del paquete
+    try {
+        const response = await axios.get(`https://registry.npmjs.org/${dependency}`);
+        const versions = Object.keys(response.data.versions || {});
 
-    // Encontrar la versión más reciente
-    const latestVersion = versions.pop();
+        // Encontrar la versión más reciente
+        const latestVersion = versions.pop();
 
-    // Ejecutar el comando para actualizar la dependencia
-    exec(`npm install ${dependency}@${latestVersion}`, (error, stdout, stderr) => {
-      if (error) {
-        console.error(`Error al actualizar ${dependency}: ${error.message}`);
-      } else {
-        console.log(`Se ha actualizado ${dependency} a la versión ${latestVersion}`);
-      }
+        // Ejecutar el comando para actualizar la dependencia
+        exec(`npm install ${dependency}@${latestVersion}`, (error, stdout, stderr) => {
+            if (error) {
+                console.error(`Error al actualizar ${dependency}: ${error.message}`);
+            } else {
+                console.log(`Se ha actualizado ${dependency} a la versión ${latestVersion}`);
+            }
 
-      // Continuar con la siguiente dependencia
-      updateDependency(index + 1);
-    });
-  } catch (error) {
-    console.error(`Error al obtener información sobre ${dependency}: ${error.message}`);
-    // Continuar con la siguiente dependencia si ocurre un error
-    updateDependency(index + 1);
-  }
+            // Continuar con la siguiente dependencia
+            updateDependency(index + 1);
+        });
+    } catch (error) {
+        console.error(`Error al obtener información sobre ${dependency}: ${error.message}`);
+        // Continuar con la siguiente dependencia si ocurre un error
+        updateDependency(index + 1);
+    }
 };
 
 // Comenzar a actualizar las dependencias desde la posición 0
