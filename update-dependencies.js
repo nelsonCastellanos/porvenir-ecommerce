10ec2632cc861c456c16f08ec6bf9d228a9614f8
Nelson Castellanos <nelson.castellanos@mercadolibre.com.co>
2023-07-22 00:06:34 -0500

    <type>: <description>
    <body>
    
    <footer>

--- update-dependencies.js
+++ update-dependencies.js
@@ -10,15 +10,36 @@ const dependencies = Object.keys(packageJson.dependencies || {});
 const devDependencies = Object.keys(packageJson.devDependencies || {});
 
 // Lista de dependencias a ignorar (puedes agregar las que desees)
-const ignoreDependencies = ["@fontsource/poppins","autosuggest-highlight","aws-sdk","axios","bcryptjs","bootstrap","compression","connected-react-router","cors","crypto","dompurify","dotenv","express","font-awesome","helmet","history","jsonwebtoken","mailchimp-api-v3","mailgun-js","mobile-detect","mongoose","mongoose-slug-generator","multer","node-sass","passport","passport-facebook","passport-google-oauth2","passport-jwt","rc-slider"];
+const ignoreDependencies = ["@fontsource/poppins","autosuggest-highlight",
+"aws-sdk","axios","bcryptjs","bootstrap","compression","connected-react-router"
+,"cors","crypto","dompurify","dotenv","express","font-awesome","helmet","history",
+"jsonwebtoken","mailchimp-api-v3","mailgun-js","mobile-detect","mongoose",
+"mongoose-slug-generator","multer","node-sass","passport","passport-facebook",
+"passport-google-oauth2","passport-jwt","rc-slider"];
 
 // Combinar todas las dependencias
-const allDependencies = [...dependencies, ...devDependencies];
+const allDependencies = [...devDependencies, ...dependencies];
 
 // Filtrar las dependencias ignoradas
 const dependenciesToUpdate = allDependencies.filter(
     (dependency) => !ignoreDependencies.includes(dependency)
 );
+
+async function getStableDependency(dependency) {
+    try {
+      const response = await axios.get(`https://registry.npmjs.org/${dependency}`);
+      if (response.status === 200 && response.data['dist-tags'] && response.data['dist-tags'].latest) {
+        const latestStableVersion = response.data['dist-tags'].latest;
+        console.log(`La última versión estable de ${dependency} es: ${latestStableVersion}`);
+        return latestStableVersion;
+      } else {
+        console.log(`No se pudo obtener la última versión estable de ${dependency}.`);
+      }
+    } catch (error) {
+      console.error('Error al obtener la dependencia:', error.message);
+    }
+    return "latest";
+}
 
 
 // Función para actualizar una dependencia y continuar con la siguiente
@@ -34,9 +55,10 @@ const updateDependency = async (index) => {
     try {
         const response = await axios.get(`https://registry.npmjs.org/${dependency}`);
         const versions = Object.keys(response.data.versions || {});
+        
 
         // Encontrar la versión más reciente
-        const latestVersion = versions.pop();
+        const latestVersion = getStableDependency(dependency);
 
         // Ejecutar el comando para actualizar la dependencia
         exec(`npm install ${dependency}@${latestVersion}`, (error, stdout, stderr) => {
@@ -45,7 +67,6 @@ const updateDependency = async (index) => {
             } else {
                 console.log(`Se ha actualizado ${dependency} a la versión ${latestVersion}`);
             }
-
             // Continuar con la siguiente dependencia
             updateDependency(index + 1);
         });
