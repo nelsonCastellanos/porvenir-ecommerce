f6631ff4247c4fd562c3b0eb4e2c727392336c46
Nelson Castellanos <nelson.castellanos@mercadolibre.com.co>
2023-09-07 23:40:20 -0500

    feat: load images from s3

--- client/app/containers/Homepage/index.js
+++ client/app/containers/Homepage/index.js
@@ -37,14 +37,14 @@ class Homepage extends React.PureComponent {
           </Col>
           <Col xs='12' lg='12' className='order-lg-1 mb-3 px-3 px-md-2'>
             <div className='d-flex flex-column h-100 justify-content-between'>
-              <img src='/images/banners/banner-2.jpeg' className='mb-3' />
-              <img src='/images/banners/banner-5.jpeg' />
+              <img src='https://elporvenir-ecommerce-content.s3.amazonaws.com/home/banner-2.jpeg' className='mb-3' />
+              <img src='https://elporvenir-ecommerce-content.s3.amazonaws.com/home/banner-5.jpeg' />
             </div>
           </Col>
           <Col xs='12' lg='12' className='order-lg-3 mb-3 px-3 px-md-2'>
             <div className='d-flex flex-column h-100 justify-content-between'>
-              <img src='/images/banners/banner-2.jpeg' className='mb-3' />
-              <img src='/images/banners/banner-6.jpeg' />
+              <img src='https://elporvenir-ecommerce-content.s3.amazonaws.com/home/banner-2.jpeg' className='mb-3' />
+              <img src='https://elporvenir-ecommerce-content.s3.amazonaws.com/home/banner-6.jpeg' />
             </div>
           </Col>
         </Row>
